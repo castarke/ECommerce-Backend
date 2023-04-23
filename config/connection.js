858facbcfea7d910956f548a55de83f358a65c3c
castarke <candler.starke@yahoo.com>
2023-04-22 23:17:38 -0400

    working on code and debugging

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,9 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('ecommerce_db', 'root', '2001Jeep', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,28 +5,83 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
 // 
-  const categoryData = await Category.findAll();
+try{
+  const categoryData = await Category.findAll(
+    {
+      include: [{
+        model: Product
+      }],
+    }
+  )
+  res.status(200).json(categoryData);
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json({message:'error in getting the categories'})
+}});
 
-  return res.json(categoryData)
-});
 
 router.get('/:id', async (req, res) => {
 // 
-  const categoryData = await Category.findByPk(req.params.id);
+try{
+  const categoryData = await Category.findByPk(req.params.id, {
+    include:[{
+      model: Product
+    }],
+  }
+  )
+  res.status(200).json(categoryData);
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json('error in getting the category')
+  }});
 
-  return res.json(categoryData)
-});
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-});
+  try{
+const categoryData = await Category.create({
+  category_name:req.body.category_name});
+  res.status(200).json(categoryData);
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json('error in creating the category')
+}});
 
-router.put('/:id', (req, res) => {
+
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-});
+  try{
+  const categoryData = await Category.update({
+    category_name: req.body.category_name
+    },{
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  res.status(200).json(categoryData);
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json('error in updating the category')
+}});
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-});
+  try{
+  const categoryData = await Category.destroy({
+    where: { 
+      id: req.params.id
+    }
+  });
+  res.status(200).json(categoryData);
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json('error in deleting the category')
+}});
 
 module.exports = router;
