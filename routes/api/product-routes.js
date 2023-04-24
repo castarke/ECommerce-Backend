a73e7582e7fd3a6fbf421b6d8b684ad194e4c06e
castarke <candler.starke@yahoo.com>
2023-04-23 22:53:37 -0400

    fixed bugs

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -54,14 +54,13 @@ catch(err){
 // create new product
 router.post('/', (req, res) => {
   //  req.body should look like this...
-  Product.create(
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    })
-
+    // {
+    //   product_name: "Basketball",
+    //   price: 200.00,
+    //   stock: 3,
+    //   tagIds: [1, 2, 3, 4]
+    // }
+  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
